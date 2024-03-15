fdd8aa83f4bb4e2386ed380ddee3c6199c5a7f48
ahmed mahmoud <ahmedmhmoud611@hotmail.com>
2024-03-15 15:46:58 +0200

    Update SearchAssets.ps1

--- 00 - Resource Deployment/SearchAssets.ps1
+++ 00 - Resource Deployment/SearchAssets.ps1
@@ -41,23 +41,23 @@ function CreateSearchIndex
         $dataSourceBody = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure-Samples/azure-search-knowledge-mining/main/00%20-%20Resource%20Deployment/templates/base-datasource.json").Content
         $dataSourceBody = $dataSourceBody -replace "{{env_storage_connection_string}}", $storageConnectionString      
         $dataSourceBody = $dataSourceBody -replace "{{env_storage_container}}", $storageContainerName        
-        CallSearchAPI -url ("/datasources/"+$dataSourceName+"?api-version=2020-06-30") -body $dataSourceBody
+        CallSearchAPI -url ("/datasources/"+$dataSourceName+"?api-version=2023-11-01") -body $dataSourceBody
 
         # Create the skillset
         $skillBody = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ahmedmahmoudR/azure-search-knowledge-mining/main/00%20-%20Resource%20Deployment/templates/base-skills.json").Content
         $skillBody = $skillBody -replace "{{cog_services_key}}", $cogServicesKey
-        CallSearchAPI -url ("/skillsets/"+$skillsetName+"?api-version=2020-06-30") -body $skillBody
+        CallSearchAPI -url ("/skillsets/"+$skillsetName+"?api-version=2023-11-01") -body $skillBody
 
         # Create the index
         $indexBody = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ahmedmahmoudR/azure-search-knowledge-mining/main/00%20-%20Resource%20Deployment/templates/base-index.json").Content
-        CallSearchAPI -url ("/indexes/"+$indexName+"?api-version=2020-06-30") -body $indexBody
+        CallSearchAPI -url ("/indexes/"+$indexName+"?api-version=2023-11-01") -body $indexBody
         
         # Create the indexer
         $indexerBody = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure-Samples/azure-search-knowledge-mining/main/00%20-%20Resource%20Deployment/templates/base-indexer.json").Content
         $indexerBody = $indexerBody -replace "{{datasource_name}}", $dataSourceName
         $indexerBody = $indexerBody -replace "{{skillset_name}}", $skillsetName   
         $indexerBody = $indexerBody -replace "{{index_name}}", $indexName   
-        CallSearchAPI -url ("/indexers/"+$indexerName+"?api-version=2020-06-30") -body $indexerBody
+        CallSearchAPI -url ("/indexers/"+$indexerName+"?api-version=2023-11-01") -body $indexerBody
 	}
     
 CreateSearchIndex;
